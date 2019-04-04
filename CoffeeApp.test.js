1ea315b9469a1b17b6361e82b133081c7ef0c445
bogdanterzea <49044962+bogdanterzea@users.noreply.github.com>
2019-04-04 13:18:19 +0300

    Unit Test
    I created the test using jest.
    I still need some advice about the result...

---
+++ CoffeeApp.test.js
@@ -0,0 +1,8 @@
+const { rld } = require('./CoffeeApp');
+
+test('Verifica Starbuksurile apropiate', () => {
+    const text = data(47.6, -122.4, coffee_shops.csv);
+expect(text).toBe(Starbucks Seattle2,4.958072512963875
+StarbucksSeattle,6.647772737097267
+Starbucks SF,1120.7577021100158);
+});
